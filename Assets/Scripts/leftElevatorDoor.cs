9a2a5972f75ffa6477a7e906797119aaa7acf425
iferezan <iferezan@smallfootprint.com>
2017-03-15 17:55:05 +0200

    added elevator and buildings

---
+++ Assets/Scripts/leftElevatorDoor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leftElevatorDoor : MonoBehaviour {
+	public float speed = 0.05F;
+	private Vector3 MovingDirection = Vector3.back;
+
+	// Update is called once per frame	
+	void Update() {		
+
+		gameObject.transform.Translate(MovingDirection * speed); ///Time.smoothDeltaTime
+         
+	    if(gameObject.transform.position.x > 0.5){
+	        MovingDirection = Vector3.back;
+	    }else if (gameObject.transform.position.x < -0.5) {
+	        MovingDirection = Vector3.forward;			
+	    }
+    }
+	
+}
---
+++ Assets/Scripts/rightElevatorDoor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rightElevatorDoor : MonoBehaviour {
+	
+
+	public float speed = 0.05F;
+	private Vector3 MovingDirection = Vector3.forward;
+
+	// Update is called once per frame	
+	void Update() {		
+		
+
+		gameObject.transform.Translate(MovingDirection * speed); ///Time.smoothDeltaTime
+         
+	    if(gameObject.transform.position.x < 1.5){
+	        MovingDirection = Vector3.forward;			
+	    }else if (gameObject.transform.position.x > 2.5) {
+	        MovingDirection = Vector3.back;			
+	    }
+    }
+	
+}
