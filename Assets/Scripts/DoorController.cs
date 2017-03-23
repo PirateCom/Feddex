27e6604807cac2ef52ef3ebe8ca44de1b0e3d918
Nistor Vlad <vnistor@SFPMacVN.local>
2017-03-23 22:46:04 +0200

    added trigger handler

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -4,31 +4,17 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class DoorController : MonoBehaviour {
-	private SteamVR_Controller.Device applicationMenuButton = Valve.VR.EVRButtonId.k_EButton_ApplicationMenu;
-	public bool applicationMenuButtonUp = false;
-	private SteamVR_Controller.Device controller {
-		get {
-			return SteamVR_Controller.Input ((init)trackedObject.index);
-		}
-	}
-	private SteamVR_TrackedObject trackedObject;
 
 	// Use this for initialization
 	void Start () {
-		trackedObject = GetComponent<SteamVR_TrackedObject> ();
+		var trackedController = GetComponent<SteamVR_TrackedController> ();
+		if (trackedController == null) {
+			trackedController = gameObject.AddComponent<SteamVR_TrackedController> ();
+		}
+		trackedController.TriggerClicked += new ClickedEventHandler (DoClick);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		if (controller == null) {
-			Debug.Log("Controller not initialized!");
-			return;
-		}
-
-		applicationMenuButtonUp = controller.GetPressUp (applicationMenuButton);
-		if (applicationMenuButtonUp == true) {
-			Debug.Log ("Door pressed");
-			SceneManager.LoadScene ("menu", LoadSceneMode.Single);
-		}
+	void DoClick(object sender, ClickedEventArgs e) {
+		Debug.Log ("DoorController::TriggerClicked");
 	}
 }
