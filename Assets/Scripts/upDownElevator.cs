bfc31f1b9c710ece2904f82edf5bcb4fb27db88d
iferezan <iferezan@smallfootprint.com>
2017-03-23 16:58:23 +0200

    elevator up

--- Assets/Scripts/upDownElevator.cs
+++ Assets/Scripts/upDownElevator.cs
@@ -1,16 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class upDownElevator : MonoBehaviour {
+public class upDownElevator : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //get up down button
+    public Button upButton;
+    public float speed = 0.2F;
+    private float distance = 0;
+
+    public Vector3 endMarker = new Vector3(0F, 69F, 19.3F);
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (distance < endMarker.y)
+        {
+            transform.Translate(Vector3.up * speed, Space.Self);
+            distance = distance + Vector3.up.y * speed;
+        }
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+    }
+
 }
