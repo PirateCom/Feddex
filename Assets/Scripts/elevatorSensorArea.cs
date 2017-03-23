50be66fe4ef1f66739791708944c1c3f8048dc10
Ioan Ferezan <iferezan@RONET1.LOCAL>
2017-03-23 16:48:14 +0200

    fix elevator dors

--- Assets/Scripts/elevatorSensorArea.cs
+++ Assets/Scripts/elevatorSensorArea.cs
@@ -9,46 +9,44 @@ public class elevatorSensorArea : MonoBehaviour {
 	public GameObject rightDoor;
 
 	public float hoverForce = 12f;
-
-	private Vector3 startPos;
-	private Vector3 endPos;
 	private float distance = 30f;
 
-	private float lerpTime = 5;
-	private float currentLerpTime = 0;
+	private float lerpTime = 50;
+	private float currentLerpTime = 50;
 
-	void start(){
-		//leftDoor = GameObject.FindGameObjectWithTag("leftDoor");
-		//rightDoor = GameObject.FindGameObjectWithTag("rightDoor");
+	Vector3 startPosLeft = new Vector3 (-0.5f, 0f,-1f);
+	Vector3 endPosLeft = new Vector3 (0.5f, 0f,-1f);
 
-		startPos = leftDoor.transform.position;
-		endPos = leftDoor.transform.position + Vector3.up * distance;
+	Vector3 startPosRight = new Vector3 (2.5f, 0f, -1f);
+	Vector3 endPosRight = new Vector3 (1.5f, 0f, -1f);
+
+	void Start () {
 	}
 
-	void update (){
-		currentLerpTime = Time.deltaTime;
+	// Update is called once per frame
+	void Update () {
+		currentLerpTime += 1;
 		if (currentLerpTime >= lerpTime) {
 			currentLerpTime = lerpTime;
 		}
-	}
-
-	void OnTriggerEnter (Collider other){
-		print("Entered collider");
-
-
-
-		Vector3 startPosLeft = new Vector3 (0.5f, 0f,-1f);
-		Vector3 endPosLeft = new Vector3 (-0.5f, 0f,-1f);
-
-		Vector3 startPosRight = new Vector3 (1.5f, 0f, -1f);
-		Vector3 endPosRight = new Vector3 (2.5f, 0f, -1f);
 
 		float Perc = currentLerpTime/lerpTime;
 		leftDoor.transform.localPosition = Vector3.Lerp(startPosLeft,endPosLeft, Perc);
 		rightDoor.transform.localPosition = Vector3.Lerp(startPosRight,endPosRight, Perc);
+	}
+
+
+	void OnTriggerEnter (Collider other){
+
+		endPosLeft = new Vector3 (-0.5f, 0f,-1f);
+		startPosLeft = new Vector3 (0.5f, 0f,-1f);
+
+		endPosRight = new Vector3 (2.5f, 0f, -1f);
+		startPosRight = new Vector3 (1.5f, 0f, -1f);
+
+		currentLerpTime = 0;
 
 		// leftDoor.transform.localPosition = new Vector3 (-0.5f, 0f,-1f);
-
 		// rightDoor.transform.localPosition = new Vector3 (2.5f, 0f, -1f);
 
 
@@ -63,14 +61,12 @@ public class elevatorSensorArea : MonoBehaviour {
 			currentLerpTime = lerpTime;
 		}
 
-		Vector3 endPosLeft = new Vector3 (0.5f, 0f,-1f);
-		Vector3 startPosLeft = new Vector3 (-0.5f, 0f,-1f);
+		startPosLeft = new Vector3 (-0.5f, 0f,-1f);
+		endPosLeft = new Vector3 (0.5f, 0f,-1f);
 
-		Vector3 endPosRight = new Vector3 (1.5f, 0f, -1f);
-		Vector3 startPosRight = new Vector3 (2.5f, 0f, -1f);
+		startPosRight = new Vector3 (2.5f, 0f, -1f);
+		endPosRight = new Vector3 (1.5f, 0f, -1f);
 
-		float Perc = currentLerpTime/lerpTime;
-		leftDoor.transform.localPosition = Vector3.Lerp(startPosLeft,endPosLeft, Perc);
-		rightDoor.transform.localPosition = Vector3.Lerp(startPosRight,endPosRight, Perc);
+		currentLerpTime = 0;
 	}
 }
