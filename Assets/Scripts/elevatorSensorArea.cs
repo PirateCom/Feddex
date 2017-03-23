fa545adf00ab0f46a4c74abb1172d337d2fbc874
iferezan <iferezan@smallfootprint.com>
2017-03-23 15:46:37 +0200

    elevator doors movement

---
+++ Assets/Scripts/elevatorSensorArea.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elevatorSensorArea : MonoBehaviour {
+
+
+	public GameObject leftDoor;
+	public GameObject rightDoor;
+
+	public float hoverForce = 12f;
+
+	private Vector3 startPos;
+	private Vector3 endPos;
+	private float distance = 30f;
+
+	private float lerpTime = 5;
+	private float currentLerpTime = 0;
+
+	void start(){
+		//leftDoor = GameObject.FindGameObjectWithTag("leftDoor");
+		//rightDoor = GameObject.FindGameObjectWithTag("rightDoor");
+
+		startPos = leftDoor.transform.position;
+		endPos = leftDoor.transform.position + Vector3.up * distance;
+	}
+
+	void update (){
+		currentLerpTime = Time.deltaTime;
+		if (currentLerpTime >= lerpTime) {
+			currentLerpTime = lerpTime;
+		}
+	}
+
+	void OnTriggerEnter (Collider other){
+		print("Entered collider");
+
+
+
+		Vector3 startPosLeft = new Vector3 (0.5f, 0f,-1f);
+		Vector3 endPosLeft = new Vector3 (-0.5f, 0f,-1f);
+
+		Vector3 startPosRight = new Vector3 (1.5f, 0f, -1f);
+		Vector3 endPosRight = new Vector3 (2.5f, 0f, -1f);
+
+		float Perc = currentLerpTime/lerpTime;
+		leftDoor.transform.localPosition = Vector3.Lerp(startPosLeft,endPosLeft, Perc);
+		rightDoor.transform.localPosition = Vector3.Lerp(startPosRight,endPosRight, Perc);
+
+		// leftDoor.transform.localPosition = new Vector3 (-0.5f, 0f,-1f);
+
+		// rightDoor.transform.localPosition = new Vector3 (2.5f, 0f, -1f);
+
+
+	}
+	void OnTriggerStay (Collider other){
+		other.GetComponent<Rigidbody>().AddForce(Vector3.up * hoverForce, ForceMode.Acceleration);
+	}
+
+	void OnTriggerExit(Collider other){
+		currentLerpTime = Time.deltaTime;
+		if (currentLerpTime >= lerpTime) {
+			currentLerpTime = lerpTime;
+		}
+
+		Vector3 endPosLeft = new Vector3 (0.5f, 0f,-1f);
+		Vector3 startPosLeft = new Vector3 (-0.5f, 0f,-1f);
+
+		Vector3 endPosRight = new Vector3 (1.5f, 0f, -1f);
+		Vector3 startPosRight = new Vector3 (2.5f, 0f, -1f);
+
+		float Perc = currentLerpTime/lerpTime;
+		leftDoor.transform.localPosition = Vector3.Lerp(startPosLeft,endPosLeft, Perc);
+		rightDoor.transform.localPosition = Vector3.Lerp(startPosRight,endPosRight, Perc);
+	}
+}
--- Assets/Scripts/leftElevatorDoor.cs
+++ Assets/Scripts/leftElevatorDoor.cs
@@ -3,19 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class leftElevatorDoor : MonoBehaviour {
-	public float speed = 0.05F;
-	private Vector3 MovingDirection = Vector3.back;
+	public float leftX = -0.5F;
+	public float leftY = 0F;
+	public float leftZ = -1F;
+	//private Vector3 MovingDirection = Vector3.back;
+	//public Vector3 pos;
 
-	// Update is called once per frame	
-	void Update() {		
 
-		gameObject.transform.Translate(MovingDirection * speed); ///Time.smoothDeltaTime
-         
-	    if(gameObject.transform.position.x > 0.5){
-	        MovingDirection = Vector3.back;
-	    }else if (gameObject.transform.position.x < -0.5) {
-	        MovingDirection = Vector3.forward;			
-	    }
-    }
-	
+	void OnMouseUp ()
+	{
+		transform.localPosition = new Vector3 (leftX, leftY, leftZ);
+	}
 }
