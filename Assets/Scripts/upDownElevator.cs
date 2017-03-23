d175be0694151404dcb66568474142226e46e50c
Ioan Ferezan <iferezan@RONET1.LOCAL>
2017-03-23 21:28:21 +0200

    Add scripts from the other branch

--- Assets/Scripts/upDownElevator.cs
+++ Assets/Scripts/upDownElevator.cs
@@ -1,35 +1,125 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Windows.Speech;
+using System.Linq;
 
 public class upDownElevator : MonoBehaviour
 {
 
     //get up down button
     public Button upButton;
-    public float speed = 0.2F;
+    public float speed = 0.02F;
     private float distance = 0;
 
-    public Vector3 endMarker = new Vector3(0F, 69F, 19.3F);
+    private Vector3 endMarkerUp = new Vector3(0F, 69F, 0F);
+    private Vector3 endMarkerDown = new Vector3(0F, 0F, 0F);
+
+    // Moving flags
+    private bool moveUp = false;
+    private bool moveDown = false;
+
+    // Voice Recognition
+	KeywordRecognizer keywordRecognizer;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
 
     // Use this for initialization
     void Start()
     {
+		print ("START");
+        //Create keywords for keyword recognizer
+        keywords.Add("go up", () =>
+        {
+            // action to be performed when this keyword is spoken
+			print("up added");
+            UpCalled();
+        });
 
+        keywords.Add("stop", () =>
+        {
+            // action to be performed when this keyword is spoken
+            StopCalled();
+        });
+
+        keywords.Add("down", () =>
+        {
+            // action to be performed when this keyword is spoken
+            DownCalled();
+        });
+
+		keywordRecognizer = new KeywordRecognizer (keywords.Keys.ToArray ());
+		keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+		keywordRecognizer.Start ();
+    }
+
+    void KeywordRecognizer_OnPhraseRecognized (PhraseRecognizedEventArgs args)
+    {
+		System.Action keywordAction;
+
+		if (keywords.TryGetValue(args.text, out keywordAction)) {
+			keywordAction.Invoke ();
+		}
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (distance < endMarker.y)
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            transform.Translate(Vector3.up * speed, Space.Self);
-            distance = distance + Vector3.up.y * speed;
+            moveDown = false;
+            moveUp = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            moveUp = false;
+            moveDown = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            moveUp = false;
+            moveDown = true;
+        }
+
+        if (moveUp == true)
+        {
+            if (distance < endMarkerUp.y)
+            {
+                transform.Translate(Vector3.up * speed, Space.Self);
+                distance = distance + Vector3.up.y * speed;
+            }
+        }
+        else if (moveDown == true)
+        {
+            if (distance > endMarkerDown.y)
+            {
+                transform.Translate(Vector3.down * speed, Space.Self);
+                distance = distance + Vector3.down.y * speed;
+            }
         }
     }
 
-    void TaskOnClick()
+    void UpCalled()
     {
-        Debug.Log("You have clicked the button!");
+        print("UP");
+        moveDown = false;
+        moveUp = true;
+    }
+
+    void StopCalled()
+    {
+        print("STOP");
+        moveDown = false;
+        moveUp = false;
+    }
+
+    void DownCalled()
+    {
+        print("DOWN");
+        moveDown = true;
+        moveUp = false;
     }
 
 }
